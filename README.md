b465d76150c5cefce6f5b760bf94a8c56c7ea1d1
mueed-shah <s45510145@sccb.ac.uk>
2021-08-16 11:14:47 +0100

    tf edited and readme updated

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ![diagram](terraform.png)
+## Launching an EC2
+- Once terraform file `main.tf` is configured we can launch the EC2 using `terraform plan` to validate the file then `terraform apply` to launch the EC2
+- EC2 instances can be destroyed using `terraform destroy`
--- main.tf
+++ main.tf
@@ -1,15 +1,32 @@
 # Lets build a script to connect to AWS nad setup all dependencies required
 # keyword: provider aws
 
- provider "aws" {
+provider "aws" {
      region = "eu-west-1"
-     
+
 }
 
-
-
-
-
-
 # then we will run terraform init
+
 # then wwill move onto launching AWS services
+
+# Lets launhc an EC2 instance in eu-west-1 with
+
+# keywith called "resource" provide resource name and give name with sspecific details to service
+resource "aws_instance" "app_instance" {
+# resource aws_ec2_instance, name it as eng89_name_terraform, ami, type of instance, with or without ip,
+ami = "ami-038d7b856fe7557b3"
+# provide type of instance 
+instance_type = "t2.micro"
+# assign public ip for this instance
+associate_public_ip_address = true
+# tags is the keyword to name the instance 
+tags = {
+    Name = "eng89_mueed_terraform"
+
+    }
+}
+
+# most commonly used commands for terraform:
+# terraform apply checks the sytnax and validates the instance 
+# once we are happy and the outcome is green we could run terraform apply
