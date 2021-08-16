72f637829a87c62c59b461f0ab7e092a7a026775
mueed-shah <s45510145@sccb.ac.uk>
2021-08-16 18:11:10 +0100

    readme + prearing for 2nd iteration

--- README.md
+++ README.md
@@ -21,3 +21,36 @@ Check and run main.tf
 `terraform apply` # Runs the main.tf file
 
 ## Ensure access and secret are added to env vars 
+
+- Lets build a script to connect to AWS nad setup all dependencies required
+- keyword: provider aws
+- then we will run terraform init
+- then wwill move onto launching AWS services
+- Lets launhc an EC2 instance in eu-west-1 with
+- keywith called "resource" provide resource name and give name with sspecific details to service
+- resource aws_ec2_instance, name it as eng89_name_terraform, ami, type of instance, with or without ip,
+- provide type of instance 
+- assign public ip for this instance
+- tags is the keyword to name the instance 
+
+
+```JSON
+provider "aws" {
+     region = "eu-west-1"
+
+}
+
+resource "aws_instance" "app_instance" {
+ami = "ami-038d7b856fe7557b3"
+instance_type = "t2.micro"
+associate_public_ip_address = true
+key_name = var.AWS_KEY_NAME
+
+tags = {
+    Name = "eng89_mueed_terraform"
+
+    }
+
+
+}
+```
--- main.tf
+++ main.tf
@@ -1,33 +1,20 @@
-# Lets build a script to connect to AWS nad setup all dependencies required
-# keyword: provider aws
-# then we will run terraform init
-# then wwill move onto launching AWS services
-# Lets launhc an EC2 instance in eu-west-1 with
-# keywith called "resource" provide resource name and give name with sspecific details to service
-# resource aws_ec2_instance, name it as eng89_name_terraform, ami, type of instance, with or without ip,
-# provide type of instance 
-# assign public ip for this instance
-# tags is the keyword to name the instance 
+# provider "aws" {
+#      region = "eu-west-1"
 
-provider "aws" {
-     region = "eu-west-1"
+# }
 
-}
+# resource "aws_instance" "app_instance" {
+# ami = "ami-038d7b856fe7557b3"
+# instance_type = "t2.micro"
+# associate_public_ip_address = true
+# key_name = var.AWS_KEY_NAME
 
-resource "aws_instance" "app_instance" {
-ami = "ami-038d7b856fe7557b3"
-instance_type = "t2.micro"
-associate_public_ip_address = true
-key_name = var.AWS_KEY_NAME
+# tags = {
+#     Name = "eng89_mueed_terraform"
 
-tags = {
-    Name = "eng89_mueed_terraform"
-
-    }
+#     }
 
 
-}
+# }
 
-# most commonly used commands for terraform:
-# terraform apply checks the sytnax and validates the instance 
-# once we are happy and the outcome is green we could run terraform apply
+
