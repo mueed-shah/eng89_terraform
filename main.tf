0e9d86997ca59ce307cce2a14634cf12b9f48799
mueed-shah <s45510145@sccb.ac.uk>
2021-08-16 10:41:06 +0100

    diagram and terraform init

---
+++ main.tf
@@ -0,0 +1,15 @@
+# Lets build a script to connect to AWS nad setup all dependencies required
+# keyword: provider aws
+
+ provider "aws" {
+     region = "eu-west-1"
+     
+}
+
+
+
+
+
+
+# then we will run terraform init
+# then wwill move onto launching AWS services
